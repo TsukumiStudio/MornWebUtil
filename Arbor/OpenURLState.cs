b8b3a3ca1e5c8ce6a6e55d981af2225da6717380
matsufriends <pikapikan0526@gmail.com>
2026-03-07 16:50:20 +0900

    Arborの移植

---
+++ Arbor/OpenURLState.cs
@@ -0,0 +1,17 @@
+﻿#if USE_ARBOR
+using Arbor;
+using UnityEngine;
+
+namespace MornLib
+{
+    public sealed class OpenURLState : StateBehaviour
+    {
+        [SerializeField] private string _url;
+
+        public override void OnStateBegin()
+        {
+            MornWebUtil.Open(_url);
+        }
+    }
+}
+#endif
