ab4f3dabeb7baec010ad9e139f17c079a897779e
matsufriends <pikapikan0526@gmail.com>
2026-05-03 07:53:51 +0900

    feat: OpenURLState を USE_MORNSTATE 対応、MornWebUtil.asmdef に MornState 参照追加

--- Arbor/OpenURLState.cs
+++ Arbor/OpenURLState.cs
@@ -1,10 +1,20 @@
-﻿#if USE_ARBOR
+#if USE_ARBOR || USE_MORNSTATE
+#if USE_MORNSTATE
+using MornLib;
+using System;
+#else
 using Arbor;
+#endif
 using UnityEngine;
 
 namespace MornLib
 {
+    [Serializable]
+#if USE_MORNSTATE
+    public sealed class OpenURLState : MornStateBehaviour
+#else
     public sealed class OpenURLState : StateBehaviour
+#endif
     {
         [SerializeField] private string _url;
 
